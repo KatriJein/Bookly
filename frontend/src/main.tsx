db854ddaa59b428371f81154bdaa7e751d55cfac
Katerina <128641157+KatriJein@users.noreply.github.com>
2025-10-14 12:08:40 +0500

    helmet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,16 @@ import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/store.ts';
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
+        <HelmetProvider>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+        </HelmetProvider>
     </StrictMode>
 );
--- frontend/src/main/page/main-page.tsx
+++ frontend/src/main/page/main-page.tsx
@@ -1,9 +1,13 @@
+import { Helmet } from 'react-helmet-async';
 import { Header } from '../../header';
 import { MainBanner } from '../banner';
 
 export function MainPage() {
     return (
-         <>
+        <>
+            <Helmet>
+                <title>Главная страница</title>
+            </Helmet>
             <Header />
             <MainBanner />
         </>
