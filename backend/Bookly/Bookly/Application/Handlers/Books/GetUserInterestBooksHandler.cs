6fc169af454c879a537ee72b45754b707de4261a
Viacheslav <Sonic.x.exe@yandex.ru>
2025-12-06 20:46:52 +0500

    улучшил метод выдачи книг в интересах пользователя

--- backend/Bookly/Bookly/Application/Handlers/Books/GetUserInterestBooksHandler.cs
+++ backend/Bookly/Bookly/Application/Handlers/Books/GetUserInterestBooksHandler.cs
@@ -13,9 +13,7 @@ namespace Bookly.Application.Handlers.Books;
 
 public class GetUserInterestBooksHandler(IMediator mediator, BooklyDbContext booklyDbContext) : IRequestHandler<GetUserInterestBooksQuery, Result<List<GetShortBookDto>>>
 {
-    private const double MinRequiredSimilarity = 0.48;
-    private const double BestSimilaritySide = 0.9;
-    private const double GoodSimilaritySide = 0.75;
+    private const double MinRequiredSimilarity = 0.2;
     
     public async Task<Result<List<GetShortBookDto>>> Handle(GetUserInterestBooksQuery request, CancellationToken cancellationToken)
     {
@@ -24,28 +22,29 @@ public class GetUserInterestBooksHandler(IMediator mediator, BooklyDbContext boo
         if (user is null) return Result<List<GetShortBookDto>>.Failure("Несуществующий или неавторизованный пользователь");
         var userGenrePreferences = await booklyDbContext.UserGenrePreferences
             .Where(p => p.UserId == request.UserId && (p.PreferenceType == PreferenceType.Liked || p.PreferenceType == PreferenceType.Neutral))
-            .Select(p => p.GenreId)
-            .ToHashSetAsync(cancellationToken);
+            .ToDictionaryAsync(p => p.GenreId, p => p.Weight, cancellationToken);
         var userAuthorPreferences = await booklyDbContext.UserAuthorPreferences
             .Where(p => p.UserId == request.UserId && (p.PreferenceType == PreferenceType.Liked ||  p.PreferenceType == PreferenceType.Neutral))
-            .Select(p => p.AuthorId)
-            .ToHashSetAsync(cancellationToken);
+            .ToDictionaryAsync(p => p.AuthorId, p => p.Weight, cancellationToken);
         if (userGenrePreferences.Count == 0 && userAuthorPreferences.Count == 0) return Result<List<GetShortBookDto>>.Success([]);
         var possibleBooks = await GetBooksInUserInterests(userGenrePreferences, userAuthorPreferences,
             user.VolumeSizePreference, user.AgeCategory, cancellationToken);
-        var relevantPossibleBooks = await mediator.Send(new ExcludeIrrelevantBooksAndEnrichRelevantWithDataCommand(possibleBooks,
+        var rearrangedBooks = RearrangeBooks(possibleBooks);
+        var relevantPossibleBooks = await mediator.Send(new ExcludeIrrelevantBooksAndEnrichRelevantWithDataCommand(rearrangedBooks,
             request.UserId, request.BookSimpleSearchSettingsDto), cancellationToken);
-        var rearrangedBooks = relevantPossibleBooks.OrderBySimilarityWeightAndShuffle(BestSimilaritySide, GoodSimilaritySide);
-        var booksDto = rearrangedBooks.Select(BookMapper.MapBookToShortBookDto).ToList();
+        var booksDto = relevantPossibleBooks.Select(BookMapper.MapBookToShortBookDto).ToList();
         return Result<List<GetShortBookDto>>.Success(booksDto);
     }
 
-    private async Task<List<Book>> GetBooksInUserInterests(HashSet<Guid> genrePreferences, HashSet<Guid> authorPreferences,
-        VolumeSizePreference? volumeSizePreference, AgeCategory? ageCategory, CancellationToken cancellationToken)
+    private async Task<List<Book>> GetBooksInUserInterests(Dictionary<Guid, double> genreWeights,
+        Dictionary<Guid, double> authorWeigths, VolumeSizePreference? volumeSizePreference, AgeCategory? ageCategory,
+        CancellationToken cancellationToken)
     {
+        var genreIds = genreWeights.Keys.ToHashSet();
+        var authorIds = authorWeigths.Keys.ToHashSet();
         var possibleInterestingBooks = await booklyDbContext.Books
             .AsNoTracking()
-            .Where(b => b.Genres.Any(g => genrePreferences.Contains(g.Id)) || b.Authors.Any(a => authorPreferences.Contains(a.Id)))
+            .Where(b => b.Genres.Any(g => genreIds.Contains(g.Id)) || b.Authors.Any(a => authorIds.Contains(a.Id)))
             .Include(b => b.Genres)
             .Include(b => b.Authors)
             .ToListAsync(cancellationToken);
@@ -53,21 +52,40 @@ public class GetUserInterestBooksHandler(IMediator mediator, BooklyDbContext boo
         {
             var bookGenres = book.Genres.Select(g => g.Id).ToHashSet();
             var bookAuthors = book.Authors.Select(a => a.Id).ToHashSet();
-            var userSuitableGenrePreferences = genrePreferences.Intersect(bookGenres).ToHashSet();
-            var userSuitableAuthorPreferences = authorPreferences.Intersect(bookAuthors).ToHashSet();
+            var averageGenresWeightSum = CalculateAverageWeight(bookGenres, genreWeights);
+            var averageAuthorsWeightSum = CalculateAverageWeight(bookAuthors, authorWeigths);
             var bookVolumeSize = BookUtils.GetVolumeSizeDependingOnPagesCount(book.PageCount);
             var bookAgeCategory = BookUtils.GetAgeCategoryDependingOnAgeRestriction(book.AgeRestriction);
-            var genresJaccard = Utils.CalculateJaccard(userSuitableGenrePreferences, bookGenres);
-            var authorsJaccard = Utils.CalculateJaccard(userSuitableAuthorPreferences, bookAuthors);
             var volumeValue = bookVolumeSize == volumeSizePreference ? 1 : 0;
             var ageCategoryValue = bookAgeCategory == ageCategory ? 1 : 0;
-            var similarity = SimilarityScores.Scores[BookSimilarityType.ByAuthor] * authorsJaccard
-                + SimilarityScores.Scores[BookSimilarityType.ByGenre] * genresJaccard
+            var similarity = SimilarityScores.Scores[BookSimilarityType.ByAuthor] * averageAuthorsWeightSum
+                + SimilarityScores.Scores[BookSimilarityType.ByGenre] * averageGenresWeightSum
                 + SimilarityScores.Scores[BookSimilarityType.ByAgeRestriction] * ageCategoryValue
                 + SimilarityScores.Scores[BookSimilarityType.ByVolumeSize] * volumeValue;
             book.SimilarityWeight = Utils.NormalizeSimilarity(similarity, SimilarityScores.MaxPossibleScoreForUserInterestBooksHandler);
         }
         return possibleInterestingBooks.Where(b => b.SimilarityWeight >= MinRequiredSimilarity).ToList();
+    }
+
+    private List<Book> RearrangeBooks(List<Book> books)
+    {
+        return books
+            .GroupBy(b => Math.Min(9, (int)Math.Floor(b.SimilarityWeight * 10)))
+            .OrderByDescending(g => g.Key)
+            .Select(g => g.OrderBy(_ => Guid.NewGuid()))
+            .SelectMany(g => g)
+            .ToList();
+    }
+    
+    private static double CalculateAverageWeight(HashSet<Guid> entitiesIds, Dictionary<Guid, double> weightsTable)
+    {
+        var values = weightsTable
+            .Where(i => entitiesIds.Contains(i.Key))
+            .Select(i => i.Value)
+            .DefaultIfEmpty(0)
+            .ToList();
+        if (values.Count != 0) return values.Average();
+        return 0;
     }
 }
 
